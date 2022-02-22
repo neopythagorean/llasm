ed367b1570384b394929299d0983e2d524e32ebc
neopythagorean <flavivs.clavdivs.jvlianvs@gmail.com>
2022-02-22 14:04:51 -0500

    filter empty && move labels down to next line

--- lasm.hs
+++ lasm.hs
@@ -9,6 +9,8 @@ import System.Environment
 
 data Token = LabelToken String | OpcodeToken String | RegisterToken String | ImmediateToken String | AssemblerToken String deriving (Show)
 
+data Register = Register { number :: Int } deriving (Show, Eq, Read)
+
 opcodes = [ "add"
           , "and"
           , "br"
@@ -35,8 +37,10 @@ main = do
         contents <- readFile $ head args
         let srclines = lines contents
         let tokenlines = filter (not . null) $ map (tokenizeLine . removeComments) srclines
-        mapM_ print $ map isOpcodeLine tokenlines
-        mapM_ print tokenlines
+        --mapM_ print $ map isSignificantLine tokenlines
+        --mapM_ print tokenlines
+        let corrected = moveLabelsDown tokenlines
+        mapM_ print corrected
 
 printUsage :: IO ()
 printUsage = do
@@ -62,9 +66,39 @@ isOpcodeToken :: Token -> Bool
 isOpcodeToken (OpcodeToken _) = True
 isOpcodeToken _ = False
 
+isAssemblerLine :: [Token] -> Bool
+isAssemblerLine = (foldl (||) False) . (map isAssemblerToken)
+
+isAssemblerToken :: Token -> Bool
+isAssemblerToken (AssemblerToken _) = True
+isAssemblerToken _ = False
+
+isLabelLine :: [Token] -> Bool
+isLabelLine = (foldl (||) False) . (map isLabelToken)
+
+isLabelToken :: Token -> Bool
+isLabelToken (LabelToken _) = True
+isLabelToken _ = False
+
+isSignificantLine :: [Token] -> Bool
+isSignificantLine l = (isOpcodeLine l) || (isAssemblerLine l)
+
 removeComments :: String -> String
 removeComments s = let h (Just k) = take k s
                        h Nothing = s
                    in h $ elemIndex ';' s
+
+-- Removes lone labels
+moveLabelsDown :: [[Token]] -> [[Token]]
+moveLabelsDown prg = let h [] _ p' = reverse p' -- Hacky reverse, fix later?
+                         h (x:p) l p'
+                             | isSignificantLine x = h p [] ( (l ++ x) : p')
+                             | isLabelLine x = h p ((filter isLabelToken x) ++ l) p'
+                             | otherwise = h p l (x:p')
+                     in h prg [] []
+
+-- Need to add checking here to make sure r <= 7
+stringToRegister :: String -> Register
+stringToRegister s = Register { number = read $ tail s :: Int }
 
 
