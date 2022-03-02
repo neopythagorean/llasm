39e00fce0813c036d6d01ee2417e291f271fc6c8
neopythagorean <flavivs.clavdivs.jvlianvs@gmail.com>
2022-03-02 09:31:30 -0500

    tokens -> ([Label], Line)

--- lasm.hs
+++ lasm.hs
@@ -6,31 +6,32 @@
 import Data.List
 import Data.Char
 import Data.Typeable
-
 import Data.Monoid
-
 import Data.Bits
-
+import Data.Maybe
 import System.Environment
+import Control.Monad
 
 data Token = LabelToken { str :: String } | OpcodeToken { str :: String } | RegisterToken { str :: String } | ImmediateToken { str :: String } | AssemblerToken { str :: String } deriving ( Show )
 
 data Register = Register { reg :: Int } deriving ( Show, Eq )
 
-data Label = Label { text :: String, resolution :: Int } deriving ( Show, Eq )
+data Label = Label { text :: String } deriving ( Show, Eq )
 
 data Immediate = Immediate { imm :: Int } deriving ( Show, Eq )
 
-data Argument = ImmArg Immediate | RegArg Register deriving ( Show, Eq )
+data Argument = ImmArg Immediate | RegArg Register | LabelArg Label deriving ( Show, Eq )
 
--- Label Symbol
-data Symbol = Symbol { symb :: String } deriving ( Show, Eq )
+-- Label Symbol (Same as Label! Resolve this!!)
+-- data Symbol = Symbol { symb :: String } deriving ( Show, Eq )
 
 data Instruction = ADD | AND | BR | JMP | JSR | JSRR | LD | LDI | LDR | LEA | NOT | RET | RTI | ST | STI | STR | TRAP deriving ( Show, Eq )
 
-data AssemlerDirection = ORIGIN | PUT deriving ( Show, Eq )
+data AssemblerDirection = ORIGIN | PUT deriving ( Show, Eq )
 
-data Line = InstLine Instruction [Argument] | AssemLine AssemlerDirection [Argument] deriving ( Show, Eq )
+data Line = InstLine Instruction [Argument] | AssemLine AssemblerDirection [Argument] deriving ( Show, Eq )
+
+--data HypotheticalLine = ([Label], (Either Instruction AssemblerDirection), [Either Label Argument])
 
 main :: IO ()
 main = do
@@ -44,7 +45,9 @@ main = do
         --mapM_ print tokenlines
         let corrected = moveLabelsDown tokenlines
         let labels = getLabels corrected
+        let asmlines = map tokensToLine corrected
         mapM_ print corrected
+        print asmlines
         print labels
 
 printUsage :: IO ()
@@ -66,21 +69,21 @@ tokenize s
     | otherwise = error ("Unable to tokenize string \"" ++ s ++ "\"")
 
 isOpcodeLine :: [Token] -> Bool
-isOpcodeLine = (foldl (||) False) . (map isOpcodeToken)
+isOpcodeLine = any isOpcodeToken
 
 isOpcodeToken :: Token -> Bool
 isOpcodeToken (OpcodeToken _) = True
 isOpcodeToken _ = False
 
 isAssemblerLine :: [Token] -> Bool
-isAssemblerLine = (foldl (||) False) . (map isAssemblerToken)
+isAssemblerLine = any isAssemblerToken
 
 isAssemblerToken :: Token -> Bool
 isAssemblerToken (AssemblerToken _) = True
 isAssemblerToken _ = False
 
 isLabelLine :: [Token] -> Bool
-isLabelLine = (foldl (||) False) . (map isLabelToken)
+isLabelLine = any isLabelToken
 
 isLabelToken :: Token -> Bool
 isLabelToken (LabelToken _) = True
@@ -108,7 +111,7 @@ stringToRegister :: String -> Register
 stringToRegister s = Register { reg = read $ tail s :: Int }
 
 stringToLabel :: String -> Label
-stringToLabel s = Label { text = (init s), resolution = 0 }
+stringToLabel s = Label { text = (init s) }
 
 -- Get all labels defined in the program
 getLabels :: [[Token]] -> [Label]
@@ -146,9 +149,9 @@ lineSize (AssemLine _ _) = 1
 
 lineToML :: Line -> [Int]
 lineToML (InstLine ADD (RegArg (Register dr) : RegArg (Register sr1) : RegArg (Register sr2) : [])) = [ constructBinary [(0b0001, 4), (dr, 3), (sr1, 3), (0b000, 3), (sr2, 3)] ]
-lineToML (InstLine ADD (RegArg (Register dr) : RegArg (Register sr) : ImmArg (Immediate i) : [])) = [ constructBinary [(0b0001, 4), (dr, 3), (sr, 3), (0b1, 1), (i, 5)] ]
-lineToML (InstLine _ _)  = [0b1111000011110000]
+lineToML (InstLine ADD (RegArg (Register dr) : RegArg (Register sr)  : ImmArg (Immediate i)  : [])) = [ constructBinary [(0b0001, 4), (dr, 3), (sr, 3), (0b1, 1), (i, 5)] ]
 lineToML (AssemLine _ _) = [0b1010111101010000]
+lineToML _ = error ("Unable to convert to ML!")
 
 -- Get only the last s bits of n
 lastBits :: Int -> Int -> Int
@@ -159,6 +162,75 @@ constructBinary :: [(Int, Int)] -> Int
 constructBinary b = let h ((n, s):xs) o = h xs ((shiftL o s) .|. (lastBits n s))
                         h [] o = o
                     in h b 0
+
+
+-- TODO
+tokensToLine :: [Token] -> ([Label], Line)
+tokensToLine l = let labels = map tokenToLabel $ filter isLabelToken l -- List of labels defined on this line
+                     filt = filter (not . isLabelToken) l    -- List of Tokens w/o the labels
+                     args = map tokenToArgument . tail
+                 in (labels, (either (\a -> (AssemLine a (args filt))) (\i -> (InstLine i (args filt))) (tokenToOperation $ head filt)))
+
+tokenToLabel :: Token -> Label
+tokenToLabel (LabelToken s) = Label (init s)
+tokenToLabel _ = error ("Cannot create label!")
+
+tokenToArgument :: Token -> Argument
+tokenToArgument (RegisterToken s) = RegArg $ Register $ stringToNumeral $ tail s
+tokenToArgument (ImmediateToken s)
+    | isDigit $ head $ tail s = ImmArg $ Immediate $ stringToNumeral $ tail s
+    | otherwise = LabelArg $ Label $ tail s
+tokenToArgument _ = error ("Cannot convert token to argument!")
+
+tokenToOperation :: Token -> Either AssemblerDirection Instruction
+tokenToOperation (OpcodeToken    op) = Right $ tokenToInstruction (OpcodeToken op)
+tokenToOperation (AssemblerToken as) = Left $ tokenToAssemblerDirection (AssemblerToken as)
+tokenToOperation _ = error ("Token is not an operation!")
+
+tokenToInstruction :: Token -> Instruction
+tokenToInstruction (OpcodeToken "add"  ) = ADD 
+tokenToInstruction (OpcodeToken "and"  ) = AND
+tokenToInstruction (OpcodeToken "br"   ) = BR 
+tokenToInstruction (OpcodeToken "jmp"  ) = JMP
+tokenToInstruction (OpcodeToken "jsr"  ) = JSR
+tokenToInstruction (OpcodeToken "jsrr" ) = JSRR
+tokenToInstruction (OpcodeToken "ld"   ) = LD
+tokenToInstruction (OpcodeToken "ldi"  ) = LDI
+tokenToInstruction (OpcodeToken "ldr"  ) = LDR
+tokenToInstruction (OpcodeToken "lea"  ) = LEA
+tokenToInstruction (OpcodeToken "not"  ) = NOT
+tokenToInstruction (OpcodeToken "ret"  ) = RET
+tokenToInstruction (OpcodeToken "rti"  ) = RTI
+tokenToInstruction (OpcodeToken "st"   ) = ST
+tokenToInstruction (OpcodeToken "sti"  ) = STI
+tokenToInstruction (OpcodeToken "str"  ) = STR
+tokenToInstruction (OpcodeToken "trap" ) = TRAP
+tokenToInstruction _ = error ("Cannot convert to Instruction")
+
+tokenToAssemblerDirection :: Token -> AssemblerDirection
+tokenToAssemblerDirection (AssemblerToken ".origin" ) = ORIGIN
+tokenToAssemblerDirection (AssemblerToken ".put"    ) = PUT
+tokenToAssemblerDirection t = error ("Cannot convert to Assembler Direction " ++ (str t))
+
+-- TODO
+--createSymbolTable :: [[Token]] -> [Label]
+
+
+-- Converts "Strings of the form '{digits}[base]' to Int"
+-- TODO : Add negative support
+stringToNumeral :: String -> Int
+stringToNumeral s = let numType n -- Get the base of the number
+                            | isPrefixOf "0h" n || isPrefixOf "0x" n  = "0123456789abcdef"
+                            | isPrefixOf "0o" n                       = "01234567"
+                            | isPrefixOf "0s" n                       = "012345"
+                            | isPrefixOf "0b" n                       = "01" 
+                            | (isDigit $ head n) || isPrefixOf "0d" n = "0123456789"
+                            | otherwise = error ("Cannot convert " ++ s ++ " to int")
+                        removeType q -- Remove base notation (if present)
+                            | (isDigit $ head q) && (length q) > 2 && flip elem "hxosbd" (head $ tail q) = drop 2 q
+                            | otherwise = q
+                        convnum d s = fmap sum . sequence $ zipWith (liftM2 (*)) (map (\c -> elemIndex c d) s) (reverse [Just ((length d)^x) | x<-[0..(length s - 1)]])
+                    in fromJust $ convnum (numType s) $ removeType s -- TODO Add error checking to this (handle fromJust)
 
 opcodes :: [String] 
 opcodes = [ "add"
@@ -178,5 +250,11 @@ opcodes = [ "add"
           , "sti"
           , "str"
           , "trap" ]
+
+assemblerInstructions :: [String]
+assemblerInstructions = [ "origin"
+                        , "put" ]
+
+
 
 
